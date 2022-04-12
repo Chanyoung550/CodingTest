d8b7f3fffd37a725d91921388f12160f697115a2
Chanyoung550 <wjdcksdud37@naver.com>
2022-04-12 23:46:05 +0900

    code01

---
+++ src/Algorithm001/Main_01.java
@@ -0,0 +1,15 @@
+package Algorithm001;
+
+public class Main_01 {
+
+	public static void main(String[] args) {
+		int findNumber = (int)(Math.random()*100);
+		for(int i = 0; i < 100; i++) {
+			if(i == findNumber) {
+				System.out.println(i);
+				break;
+			}
+		}
+	}
+
+}
---
+++ src/Algorithm001/Main_02.java
@@ -0,0 +1,33 @@
+package Algorithm001;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+
+//N개의 수가 주어졌을 때 이를 오름차순 정렬하는 프로그램을 작성하시오.
+
+/*입력
+ * 1번째 줄에 수의 개수 N(1<= N <=1000000), 2번째 줄부터는 N개의 줄에 숫자가 주어진다. 이 수는 절댓값이 1000000 보다 작거나  같은 정수다. 수는 중복되지 않는다.
+ * 5	5	2	3	4	1
+ */
+
+/*출력
+ * 1번째 줄부터 N개의 줄에 오름차순 정렬한 결과를 1줄에 1개씩 출력한다.
+ * 1	2	3	4	5	
+ */
+public class Main_02 {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		String line;
+		while((line = bf.readLine()) != null) {
+			
+		}
+		
+		
+		
+	}
+	
+}
